dc76480836b1a75f28610207bbd0711026b57d82
arjun1709@bitbucket.org <arjun1709@gmail.com>
2020-02-17 06:05:47 +0530

    updated variables.

--- encrypt.js
+++ encrypt.js
@@ -1,8 +1,8 @@
 const cryptojs = require('crypto-js')
 
 async function encrypt() {
-    const hashedPassword = '7EISLKt77qbP7AFBGwD2WGTjngnXn73r61SbzSTvAoARbrlfSec0bn6ileP0khHsO+GxxbU4j9z2aNK+Ayxj38NYdP0bOD1Ee20NpiaoLPLeLN35GmOly78hljT9fwZla+jBIih2c+VE8owsfrCUGv/ZLWDXXHsMtU5N8Ton6v0=';
-    const privateKey = '0x8CE0966AB535210F2E64C7CD9E5272756A6E0E73CB811E7FB6476AAB55B9ED5A'
+    const hashedPassword = 'This is the hashed password';
+    const privateKey = 'This is the private key'
 
     var encrypt = cryptojs.AES.encrypt(privateKey, hashedPassword)
     console.log("Encrypted Private Key", encrypt.toString())
